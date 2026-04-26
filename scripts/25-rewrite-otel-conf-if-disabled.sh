581e490f305fa80170247f789972657495ad9393
Vladislav Yatsun <vyatsun@deltixlab.com>
2026-04-26 23:18:41 +0200

    feat: fine tune nginx configuration

--- scripts/25-rewrite-otel-conf-if-disabled.sh
+++ scripts/25-rewrite-otel-conf-if-disabled.sh
@@ -4,18 +4,16 @@ set -e
 
 ME="$(basename "$0")"
 
-
 entrypoint_log() {
     if [ -z "${NGINX_ENTRYPOINT_QUIET_LOGS:-}" ]; then
         echo "$@"
     fi
 }
 
-
 rewrite_otel_conf_if_disabled() {
     if [ -z "${OTEL_EXPORTER_OTLP_ENDPOINT:-}" ]; then
         entrypoint_log "$ME: OTEL_EXPORTER_OTLP_ENDPOINT is not set. Rewrite /tmp/otel.conf"
-        cat > /tmp/otel.conf << 'EOF'
+        cat >/tmp/otel.conf <<'EOF'
 otel_trace off;
 
 otel_exporter {
